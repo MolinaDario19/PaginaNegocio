c9a59ab5f2c24d5ed86ebb6c12b10b6e9a1dbaed
MolinaDario19 <molinadario92@gmail.com>
2026-01-13 09:52:18 -0500

    Actualización copyright

---
+++ js/copyright.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+const copyright =
+    `${year} ©DigitalElectronics. Todos los derechos reservados`;
+
+document.getElementById("copyright").textContent = copyright;
